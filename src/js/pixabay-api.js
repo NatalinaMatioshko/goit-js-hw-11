6a8743c75e38e59f4afe180f0e08aeeb0c6cf948
Natalia Matioshko <165213062+NatalinaMatioshko@users.noreply.github.com>
2024-08-19 23:56:15 +0300

    miss one num)

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,6 +1,6 @@
 export default function fetchingGallery(userRequest) {
   const searchParams = new URLSearchParams({
-    key: '5524155-4a5a252fd9d205519c9b1e139',
+    key: '45524155-4a5a252fd9d205519c9b1e139',
     q: userRequest,
     image_type: 'photo',
     orientation: 'horizontal',
